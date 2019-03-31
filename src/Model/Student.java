ee074af1582c46e4258740ae5f5b03bb85e90193
Dorreen Rostami <rdorreen@yahoo.com>
2019-03-31 14:29:20 +0430

    setter and getter for student

--- src/Model/Student.java
+++ src/Model/Student.java
@@ -1,15 +1,45 @@
 package Model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Student {
-    public static final List<Student> STUDENTS = new ArrayList<>();
-
+public class Student implements Serializable {
     private String username;
     private String password;
     private long balance;
-    private int GPA;
-    private List<Course> COURSES_TAKEN = new ArrayList<>();
+    private int GPA = 20;
+    //private List<Course> COURSES_TAKEN = new ArrayList<>();
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public int getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(int GPA) {
+        this.GPA = GPA;
+    }
 }
