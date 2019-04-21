01d4f1f021e1795dca214646ee996698d4a6ea98
Dorreen Rostami <rdorreen@yahoo.com>
2019-04-21 23:58:57 +0430

    added special prices for students with higher a GPA than 17

--- src/Controller/StudentFoodController.java
+++ src/Controller/StudentFoodController.java
@@ -31,44 +31,84 @@ public class StudentFoodController {
         theBalance.setVisible(true);
         if (reservedFood.get(0) == null)
             checkBox1.setVisible(false);
-        else
-            checkBox1.setText(reservedFood.get(0).getName() + " " + reservedFood.get(0).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox1.setText(reservedFood.get(0).getName() + " " + (reservedFood.get(0).getPrice() - 100));
+            else
+                checkBox1.setText(reservedFood.get(0).getName() + " " + reservedFood.get(0).getPrice());
+        }
         if (reservedFood.get(1) == null)
             checkBox2.setVisible(false);
-        else
-            checkBox2.setText(reservedFood.get(1).getName() + " " + reservedFood.get(1).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox2.setText(reservedFood.get(1).getName() + " " + (reservedFood.get(1).getPrice() - 100));
+            else
+                checkBox2.setText(reservedFood.get(1).getName() + " " + reservedFood.get(1).getPrice());
+        }
         if (reservedFood.get(2) == null)
             checkBox3.setVisible(false);
-        else
-            checkBox3.setText(reservedFood.get(2).getName() + " " + reservedFood.get(2).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox3.setText(reservedFood.get(2).getName() + " " + (reservedFood.get(2).getPrice() - 100));
+            else
+                checkBox3.setText(reservedFood.get(2).getName() + " " + reservedFood.get(2).getPrice());
+        }
         if (reservedFood.get(3) == null)
             checkBox4.setVisible(false);
-        else
-            checkBox4.setText(reservedFood.get(3).getName() + " " + reservedFood.get(3).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox4.setText(reservedFood.get(3).getName() + " " + (reservedFood.get(3).getPrice() - 100));
+            else
+                checkBox4.setText(reservedFood.get(3).getName() + " " + reservedFood.get(3).getPrice());
+        }
         if (reservedFood.get(4) == null)
             checkBox5.setVisible(false);
-        else
-            checkBox5.setText(reservedFood.get(4).getName() + " " + reservedFood.get(4).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox5.setText(reservedFood.get(4).getName() + " " + (reservedFood.get(4).getPrice() - 100));
+            else
+                checkBox5.setText(reservedFood.get(4).getName() + " " + reservedFood.get(4).getPrice());
+        }
         if (reservedFood.get(5) == null)
             checkBox6.setVisible(false);
-        else
-            checkBox6.setText(reservedFood.get(5).getName() + " " + reservedFood.get(5).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox6.setText(reservedFood.get(5).getName() + " " + (reservedFood.get(5).getPrice() - 100));
+            else
+                checkBox6.setText(reservedFood.get(5).getName() + " " + reservedFood.get(5).getPrice());
+        }
         if (reservedFood.get(6) == null)
             checkBox7.setVisible(false);
-        else
-            checkBox7.setText(reservedFood.get(6).getName() + " " + reservedFood.get(6).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox7.setText(reservedFood.get(6).getName() + " " + (reservedFood.get(6).getPrice() - 100));
+            else
+                checkBox7.setText(reservedFood.get(6).getName() + " " + reservedFood.get(6).getPrice());
+        }
         if (reservedFood.get(7) == null)
             checkBox8.setVisible(false);
-        else
-            checkBox8.setText(reservedFood.get(7).getName() + " " + reservedFood.get(7).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox8.setText(reservedFood.get(7).getName() + " " + (reservedFood.get(7).getPrice() - 100));
+            else
+                checkBox8.setText(reservedFood.get(7).getName() + " " + reservedFood.get(7).getPrice());
+        }
         if (reservedFood.get(8) == null)
             checkBox9.setVisible(false);
-        else
-            checkBox9.setText(reservedFood.get(8).getName() + " " + reservedFood.get(8).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox9.setText(reservedFood.get(8).getName() + " " + (reservedFood.get(8).getPrice() - 100));
+            else
+                checkBox9.setText(reservedFood.get(8).getName() + " " + reservedFood.get(8).getPrice());
+        }
         if (reservedFood.get(9) == null)
             checkBox10.setVisible(false);
-        else
-            checkBox10.setText(reservedFood.get(9).getName() + " " + reservedFood.get(9).getPrice());
+        else {
+            if (signedInStudent.getGPA() > 17)
+                checkBox10.setText(reservedFood.get(9).getName() + " " + (reservedFood.get(9).getPrice() - 100));
+            else
+                checkBox10.setText(reservedFood.get(9).getName() + " " + reservedFood.get(9).getPrice());
+        }
         List<Food> studentsFoods = signedInStudent.getRESERVED_FOODS();
         for (Food f : studentsFoods) {
             if (f.getDay() == Day.shanbe) {
@@ -164,43 +204,73 @@ public class StudentFoodController {
         int total = 0;
         if(checkBox1.isSelected()){
             studentsFood.add(reservedFood.get(0));
-            total += reservedFood.get(0).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(0).getPrice() - 100;
+            else
+                total += reservedFood.get(0).getPrice();
         }
         else if(checkBox2.isSelected()){
             studentsFood.add(reservedFood.get(1));
-            total += reservedFood.get(1).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(1).getPrice() - 100;
+            else
+                total += reservedFood.get(1).getPrice();
         }
         if(checkBox3.isSelected()){
             studentsFood.add(reservedFood.get(2));
-            total += reservedFood.get(2).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(2).getPrice() - 100;
+            else
+                total += reservedFood.get(2).getPrice();
         }
         else if(checkBox4.isSelected()){
             studentsFood.add(reservedFood.get(3));
-            total += reservedFood.get(3).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(3).getPrice() - 100;
+            else
+                total += reservedFood.get(3).getPrice();
         }
         if(checkBox5.isSelected()){
             studentsFood.add(reservedFood.get(4));
-            total += reservedFood.get(4).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(4).getPrice() - 100;
+            else
+                total += reservedFood.get(4).getPrice();
         }
         else if(checkBox6.isSelected()){
             studentsFood.add(reservedFood.get(5));
-            total += reservedFood.get(5).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(5).getPrice() - 100;
+            else
+                total += reservedFood.get(5).getPrice();
         }
         if(checkBox7.isSelected()){
             studentsFood.add(reservedFood.get(6));
-            total += reservedFood.get(6).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(6).getPrice() - 100;
+            else
+                total += reservedFood.get(6).getPrice();
         }
         else if(checkBox8.isSelected()){
             studentsFood.add(reservedFood.get(7));
-            total += reservedFood.get(7).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(7).getPrice() - 100;
+            else
+                total += reservedFood.get(7).getPrice();
         }
         if(checkBox9.isSelected()){
             studentsFood.add(reservedFood.get(8));
-            total += reservedFood.get(8).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(8).getPrice() - 100;
+            else
+                total += reservedFood.get(8).getPrice();
         }
         else if(checkBox10.isSelected()){
             studentsFood.add(reservedFood.get(9));
-            total += reservedFood.get(9).getPrice();
+            if (signedInStudent.getGPA() > 17)
+                total += reservedFood.get(9).getPrice() - 100;
+            else
+                total += reservedFood.get(9).getPrice();
         }
         if (total > signedInStudent.getBalance())
             notEnoughLabel.setVisible(true);
